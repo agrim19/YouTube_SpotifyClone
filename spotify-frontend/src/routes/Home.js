06cce575a88381222fd6d88c84eeb83301d4e056
agrim19 <agrim19229@iiitd.ac.in>
2023-04-23 14:15:51 +0530

    server helpers

--- spotify-frontend/src/routes/Home.js
+++ spotify-frontend/src/routes/Home.js
@@ -3,6 +3,62 @@ import spotify_logo from "../assets/images/spotify_logo_white.svg";
 import IconText from "../components/shared/IconText";
 import TextWithHover from "../components/shared/TextWithHover";
 
+const focusCardsData = [
+    {
+        title: "Peaceful Piano",
+        description: "Relax and indulge with beautiful piano pieces",
+        imgUrl: "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1546&q=80",
+    },
+    {
+        title: "Deep Focus",
+        description: "Keep calm and focus with this music",
+        imgUrl: "https://images.unsplash.com/photo-1558021212-51b6ecfa0db9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1766&q=80",
+    },
+    {
+        title: "Instrumental Study",
+        description: "Focus with soft study music in the background.",
+        imgUrl: "https://images.unsplash.com/photo-1612225330812-01a9c6b355ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+    },
+    {
+        title: "Focus Flow",
+        description: "Up tempo instrumental hip hop beats",
+        imgUrl: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+    {
+        title: "Beats to think to",
+        description: "Focus with deep techno and tech house",
+        imgUrl: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+];
+
+const spotifyPlaylistsCardData = [
+    {
+        title: "This is one",
+        description: "Relax and indulge with beautiful piano pieces",
+        imgUrl: "https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1546&q=80",
+    },
+    {
+        title: "Deep Focus",
+        description: "Keep calm and focus with this music",
+        imgUrl: "https://images.unsplash.com/photo-1558021212-51b6ecfa0db9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1766&q=80",
+    },
+    {
+        title: "Instrumental Study",
+        description: "Focus with soft study music in the background.",
+        imgUrl: "https://images.unsplash.com/photo-1612225330812-01a9c6b355ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
+    },
+    {
+        title: "Focus Flow",
+        description: "Up tempo instrumental hip hop beats",
+        imgUrl: "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+    {
+        title: "Beats to think to",
+        description: "Focus with deep techno and tech house",
+        imgUrl: "https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+];
+
 const Home = () => {
     return (
         <div className="h-full w-full flex">
@@ -53,7 +109,7 @@ const Home = () => {
                 </div>
             </div>
             {/* This second div will be the right part(main content) */}
-            <div className="h-full w-4/5 bg-app-black">
+            <div className="h-full w-4/5 bg-app-black overflow-auto">
                 <div className="navbar w-full h-1/10 bg-black bg-opacity-30 flex items-center justify-end">
                     <div className="w-1/2 flex h-full">
                         <div className="w-3/5 flex justify-around items-center">
@@ -70,55 +126,54 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <div className="content p-8">
-                    <PlaylistView />
+                <div className="content p-8 pt-0 overflow-auto">
+                    <PlaylistView
+                        titleText="Focus"
+                        cardsData={focusCardsData}
+                    />
+                    <PlaylistView
+                        titleText="Spotify Playlists"
+                        cardsData={spotifyPlaylistsCardData}
+                    />
+                    <PlaylistView
+                        titleText="Sound of India"
+                        cardsData={focusCardsData}
+                    />
                 </div>
             </div>
         </div>
     );
 };
 
-const PlaylistView = () => {
+const PlaylistView = ({titleText, cardsData}) => {
     return (
-        <div className="text-white">
-            <div className="text-2xl font-semibold mb-5">Focus</div>
-            <div className="w-full flex justify-between">
-                <Card
-                    title="Peaceful Piano"
-                    description="Relax and indulge with beautiful piano pieces"
-                />
-                <Card
-                    title="Deep Focus"
-                    description="Keep calm and focus with this music"
-                />
-                <Card
-                    title="Instrumental Study"
-                    description="Focus with soft study music in the background."
-                />
-                <Card
-                    title="Focus Flow"
-                    description="Up tempo instrumental hip hop beats"
-                />
-                <Card
-                    title="Beats to think to"
-                    description="Focus with deep techno and tech house"
-                />
+        <div className="text-white mt-8">
+            <div className="text-2xl font-semibold mb-5">{titleText}</div>
+            <div className="w-full flex justify-between space-x-4">
+                {
+                    // cardsData will be an array
+                    cardsData.map((item) => {
+                        return (
+                            <Card
+                                title={item.title}
+                                description={item.description}
+                                imgUrl={item.imgUrl}
+                            />
+                        );
+                    })
+                }
             </div>
         </div>
     );
 };
 
-const Card = ({title, description}) => {
+const Card = ({title, description, imgUrl}) => {
     return (
-        <div className="bg-black bg-opacity-60 w-1/6 p-4 rounded-lg">
+        <div className="bg-black bg-opacity-40 w-1/5 p-4 rounded-lg">
             <div className="pb-4 pt-2">
-                <img
-                    className="w-full rounded-md"
-                    src="https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1546&q=80"
-                    alt="label"
-                />
+                <img className="w-full rounded-md" src={imgUrl} alt="label" />
             </div>
-            <div className="text-white text-sm font-semibold">{title}</div>
+            <div className="text-white font-semibold py-3">{title}</div>
             <div className="text-gray-500 text-sm">{description}</div>
         </div>
     );
---
+++ spotify-frontend/src/utils/serverHelpers.js
@@ -0,0 +1,13 @@
+import {backendUrl} from "./config";
+
+export const makeUnauthenticatedPOSTRequest = async (route, body) => {
+    const response = await fetch(backendUrl + route, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    });
+    const formattedResponse = await response.json();
+    return formattedResponse;
+};
